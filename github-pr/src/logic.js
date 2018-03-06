e16193edcc64e45afdeab0cc35c980ec4f576d6c
Chakrit <chakrit.lj@gmail.com>
2018-03-06 15:15:55 +0700

    Create warning level

--- github-pr/src/logic.js
+++ github-pr/src/logic.js
@@ -1,12 +1,24 @@
 const moment = require('moment')
 
 function isPrObsolte (pullRequest, now = moment()) {
+  return getPrWarningLevel(pullRequest, now).level === 4
+}
+
+function getPrWarningLevel (pullRequest, now = moment()) {
   if (!pullRequest || !pullRequest.updated_at) {
     throw new Error('Input is not pull request object')
   }
-  return moment(pullRequest.updated_at).isBefore(now.subtract(2, 'weeks'))
+  const dayDiff = Math.abs(moment(pullRequest.updated_at).diff(now, 'days'))
+  if (dayDiff <= 3) return { level: 0 }
+  if (dayDiff <= 7) return { level: 1 }
+  if (dayDiff <= 12) return { level: 2 }
+  if (dayDiff <= 14) return { level: 3 }
+  return {
+    level: 4
+  }
 }
 
 module.exports = {
-  isPrObsolte
+  isPrObsolte,
+  getPrWarningLevel
 }
--- github-pr/src/logic.test.js
+++ github-pr/src/logic.test.js
@@ -6,7 +6,11 @@ describe('github logic', () => {
   const assertErrMessage = (func, message) => {
     try {
       func()
+      throw new Error('AssertErrMessage')
     } catch (err) {
+      if (err.message === 'AssertErrMessage') {
+        throw Error('Function did not throw')
+      }
       assert(err.message === message, `Should throw error "${message}", instead throw "${err.message}"`)
     }
   }
@@ -29,6 +33,52 @@ describe('github logic', () => {
 
     it('should throw if object does not have updated_at (which means maybe not pr)', () => {
       assertErrMessage(() => logic.isPrObsolte(null), 'Input is not pull request object')
+    })
+  })
+
+  describe('Pull request warning level', () => {
+    it('should throw if object does not have updated_at (which means maybe not pr)', () => {
+      assertErrMessage(() => logic.getPrWarningLevel(null), 'Input is not pull request object')
+    })
+
+    it('should return level 0 if updated within 3 days', () => {
+      const now = moment('2017-07-14')
+      const pr = {
+        updated_at: moment('2017-07-12').toISOString()
+      }
+      assert(logic.getPrWarningLevel(pr, now).level === 0)
+    })
+
+    it('should return level 1 if updated more than 3 days within 7 days', () => {
+      const now = moment('2017-07-14')
+      const pr = {
+        updated_at: moment('2017-07-10').toISOString()
+      }
+      assert(logic.getPrWarningLevel(pr, now).level === 1)
+    })
+
+    it('should return level 2 if updated more than 7 days within 12 days', () => {
+      const now = moment('2017-07-14')
+      const pr = {
+        updated_at: moment('2017-07-06').toISOString()
+      }
+      assert(logic.getPrWarningLevel(pr, now).level === 2)
+    })
+
+    it('should return level 3 if updated more than 12 days within 14 days', () => {
+      const now = moment('2017-07-14')
+      const pr = {
+        updated_at: moment('2017-07-01').toISOString()
+      }
+      assert(logic.getPrWarningLevel(pr, now).level === 3)
+    })
+
+    it('should return level 4 if updated more than 14 days ago', () => {
+      const now = moment('2017-07-14')
+      const pr = {
+        updated_at: moment('2017-06-29').toISOString()
+      }
+      assert(logic.getPrWarningLevel(pr, now).level === 4)
     })
   })
 })
